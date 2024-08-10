d1587495e458013077085970a9a3fbd45e8ee94a
marionbondoc <marionjjjjjbondoc@gmail.com>
2024-08-10 17:29:31 +0800

    Add files via upload

---
+++ Server_Routing.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+    console.log("There is GET request for the homepage!");
+    res.send('Here is the GET Method!');
+});
+
+app.post('/', function(req, res){
+    console.log("A POST request for the homepage is accessed.");
+    res.send('Here is the POST Method!');
+});
+
+app.get ('/list_user' , function(req, res){
+    console.log("Got a GET request for /list_user.");
+    res.send('This is Page Listing');
+});
+
+app.get('/ab*cd', function(req, res) {
+    console.log("Got a GET request for /ab*cd");
+    res.send('Pattern Match Page');
+});
+
+app.get('/mar', function(req, res) {
+    console.log("yeah");
+    res.send('Marion Bondoc WD-303');
+});
+
+var server = app.listen(4000, function() {
+    var host = server.address().address
+    var port = server.address().port
+    
+    console.log("Example app listening at http://%s:%s", host, port)
+});
